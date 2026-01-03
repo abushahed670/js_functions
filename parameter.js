7da9dc817e5f2538a844f1cd0f4edb187240624c
abushahed670 <abu.shahed670@gmail.com>
2026-01-04 00:22:17 +0600

    parameter and arguments are added

---
+++ parameter.js
@@ -0,0 +1,14 @@
+function vaatKhabo(){
+    console.log('hand wash koreaso');
+    console.log('boso');
+    console.log('gopat gopat kore khao');
+
+}
+// vaatKhabo();
+
+function square(number){
+    console.log('value of parameter:', number);
+    const borgo= number * number;
+    console.log('square of the give number is:', borgo);
+}
+square(4);
