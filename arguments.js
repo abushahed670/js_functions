5351fa8d91a29a4293fd310785fe6846e5067551
abushahed670 <abu.shahed670@gmail.com>
2026-01-04 00:38:43 +0600

    arguments added

--- arguments.js
+++ arguments.js
@@ -8,3 +8,13 @@ console.log('..........');
 doubleIt(20);
 console.log('.........')
 doubleIt(405);
+
+
+function difference(num1, num2){
+    const diff = num1 - num2;
+    console.log(num1, num2, 'difference is:', diff);
+}
+const fatherAge= 40;
+const myAge=10;
+
+difference(fatherAge, myAge);
